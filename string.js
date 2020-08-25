baa9364e278e45d4771314ea0003a11e04593012
Harshis517 <56535203+Harshis517@users.noreply.github.com>
2020-08-25 14:11:47 +0530

    Update string.js

--- string.js
+++ string.js
@@ -1,13 +1,3 @@
 const index = require("./index");
 
-function GetallColorCodedPairs() {
-  var i;
-  var colorCodePair = [];
-  for (i = 1; i < 26; i++) {
-    var colorCombo ={}
-    colorCombo.number = i;
-    colorCombo.color = index.GetColorFromPairNumber(i);
-    colorCodePair.push(colorCombo);  
-  }
-  return colorCodePair;
-}
+function ToString ()

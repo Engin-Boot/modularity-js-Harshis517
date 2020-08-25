c3a32a8e29d7f60e381a3a8f3c028c9646874fdc
Harshis517 <56535203+Harshis517@users.noreply.github.com>
2020-08-25 14:03:48 +0530

    Create string.js

---
+++ string.js
@@ -0,0 +1,13 @@
+const index = require("./index");
+
+function GetallColorCodedPairs() {
+  var i;
+  var colorCodePair = [];
+  for (i = 1; i < 26; i++) {
+    var colorCombo ={}
+    colorCombo.number = i;
+    colorCombo.color = index.GetColorFromPairNumber(i);
+    colorCodePair.push(colorCombo);  
+  }
+  return colorCodePair;
+}
